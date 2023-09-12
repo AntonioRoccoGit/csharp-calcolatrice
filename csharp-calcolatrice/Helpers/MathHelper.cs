400934e5910722318e4c10d1d155c51dac2110a3
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-12 13:25:44 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/Helpers/MathHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice.Helpers
+{
+    public static class MathHelper
+    {
+
+
+        //INT FUNCTION
+
+        public static int IntSumm(int number1, int number2) 
+        {
+            return number1 + number2;
+        }
+        
+        public static int IntDiff(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public static int IntMultiplier(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public static   int IntAbsolute(int number1)
+        {
+            return Math.Abs(number1);
+        }
+        public static int IntMinor(int number1, int number2)
+        {
+            if(number1 < number2)
+                return number1;
+            return number2;
+        }
+        public static int IntMax(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number1;
+            return number2;
+        }
+        
+
+        //DOUBLE FUNCTION
+
+        public static double DoubleSumm(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+        public static double DoubleDiff(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+        public static double DoubleMultiplier(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+        public static double DoubleAbsolute(double number1)
+        {
+            return Math.Abs(number1);
+        }
+        public static double DoubleMinor(double number1, double number2)
+        {
+            if (number1 < number2)
+                return number1;
+            return number2;
+        }
+        public static double DoubleMax(double number1, double number2)
+        {
+            if (number1 > number2)
+                return number1;
+            return number2;
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,35 @@
+﻿using csharp_calcolatrice.Helpers;
+
+namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SUMM
+            Console.WriteLine(MathHelper.IntSumm(4, 6));
+            Console.WriteLine(MathHelper.DoubleSumm(4.832656446548, 4564654656.5));
+
+            //DIFFERENCE
+            Console.WriteLine(MathHelper.IntDiff(6,8));
+            Console.WriteLine(MathHelper.DoubleDiff(6.1567,8.9665));
+
+            //MULTIPLICATION
+            Console.WriteLine(MathHelper.IntMultiplier(3,8));
+            Console.WriteLine(MathHelper.DoubleMultiplier(3.65,4.21));
+
+            //ABSOLUTE
+            Console.WriteLine(MathHelper.IntAbsolute(-6));
+            Console.WriteLine(MathHelper.DoubleAbsolute(-42.12));
+
+            //MINOR NUMBER
+            Console.WriteLine(MathHelper.IntMinor(12,6));
+            Console.WriteLine(MathHelper.DoubleMinor(10.22,10.65));
+
+            //MAX NUMBER
+            Console.WriteLine(MathHelper.IntMax(12, 6));
+            Console.WriteLine(MathHelper.DoubleMax(10.22, 10.65));
+
+        }
+    }
+}
